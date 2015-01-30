b2240e4af863e3f7482e14ad34b118c81c749153
Hans-Peter Deifel <hans-peter.deifel@fau.de>
2015-01-30 14:29:15 +0100

    Show poppler version in --version

--- pdfgrep.cc
+++ pdfgrep.cc
@@ -399,7 +399,8 @@ void print_help(char *self)
 
 void print_version()
 {
-	printf("This is %s version %s\n", PACKAGE, VERSION);
+	printf("This is %s version %s.\n", PACKAGE, VERSION);
+	printf("\nUsing poppler version %s\n", poppler::version_string().c_str());
 	if (strcmp(PDFGREP_GIT_HEAD, "") != 0) {
 		printf("Built from git-commit %s\n", PDFGREP_GIT_HEAD);
 	}
