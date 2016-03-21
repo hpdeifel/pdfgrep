65251c3da51730be6ad9596c8081689163242177
Hans-Peter Deifel <hpd@hpdeifel.de>
2016-03-21 01:00:13 +0100

    Update copyright

--- src/search.cc
+++ src/search.cc
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2015 by Hans-Peter Deifel                               *
+ *   Copyright (C) 2016 by Hans-Peter Deifel                               *
  *   hpd@hpdeifel.de                                                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
